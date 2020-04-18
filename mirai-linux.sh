1f07466f1573dbe4174e0233453fd53adfc5a53c
Cyan <a484599279@hotmail.com>
2020-04-19 02:42:55 +0800

    支持 centos

--- mirai-linux.sh
+++ mirai-linux.sh
@@ -7,15 +7,14 @@ plain='\033[0m'
 
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
 
-
 download() {
     local filename=$(basename $1)
     if [ -f ${1} ]; then
         echo "[Info] ${filename} [found]"
     else
         echo "[Info] ${filename} not found, download it now..."
+        wget --no-check-certificate -c -t3 -T60 -O ${1} ${2}
         # wget --no-check-certificate -q --show-progress -c -t3 -T60 -O ${1} ${2}
-        wget --no-check-certificate -q --show-progress -c -t3 -T60 -O ${1} ${2}
         if [ $? -ne 0 ]; then
             echo -e "[${red}Error${plain}] Download ${red}${filename}${plain} failed."
             exit 1
@@ -23,12 +22,46 @@ download() {
     fi
 }
 
-install_jdk() {
-    echo -e "[${yellow}Info${plain}] Starting to install ${green}openjdk-11-jdk${plain}"
+yum_install() {
+    local pkg_name=$1
+    echo -e "[${yellow}Info${plain}] Starting to install ${green}${pkg_name}${plain}"
+    yum -y install ${pkg_name} >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "[${red}Error${plain}] Failed to install ${red}${pkg_name}${plain}"
+        exit 1
+    fi
+}
+
+apt_install() {
+    local pkg_name=$1
+    echo -e "[${yellow}Info${plain}] Starting to install ${green}${pkg_name}${plain}"
     apt-get -y install openjdk-11-jdk >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo -e "[${red}Error${plain}] Failed to install ${red}openjdk-11-jdk${plain}"
+        echo -e "[${red}Error${plain}] Failed to install ${red}${pkg_name}${plain}"
         exit 1
+    fi
+}
+
+install_pkgs() {
+    if [ -x "$(command -v yum)" ]; then
+
+        yum_pkgs=(
+            wget java-11-openjdk-devel
+        )
+
+        for pkg in ${yum_pkgs[@]}; do
+            yum_install ${pkg}
+        done
+
+    elif [ -x "$(command -v apt-get)" ]; then
+        apt_pkgs=(
+            openjdk-11-jdk wget
+        )
+
+        for pkg in ${apt_pkgs[@]}; do
+            apt_install ${pkg}
+        done
+
     fi
 }
 
@@ -68,9 +101,8 @@ start_mirai() {
     java -jar ./mirai-console-wrapper-0.3.0.jar
 }
 
-
 # install process
-install_jdk
+install_pkgs
 download_files
 clean_up_files
 config_mirai
