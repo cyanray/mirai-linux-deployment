c8c440587a7e56d74f2273c20a1fd49878170c05
Cyan <a484599279@hotmail.com>
2020-04-18 19:40:19 +0800

    init

---
+++ mirai-linux.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
+
+
+download() {
+    local filename=$(basename $1)
+    if [ -f ${1} ]; then
+        echo "[Info] ${filename} [found]"
+    else
+        echo "[Info] ${filename} not found, download it now..."
+        # wget --no-check-certificate -q --show-progress -c -t3 -T60 -O ${1} ${2}
+        wget --no-check-certificate -q --show-progress -c -t3 -T60 -O ${1} ${2}
+        if [ $? -ne 0 ]; then
+            echo -e "[${red}Error${plain}] Download ${red}${filename}${plain} failed."
+            exit 1
+        fi
+    fi
+}
+
+install_jdk() {
+    echo -e "[${yellow}Info${plain}] Starting to install ${green}openjdk-11-jdk${plain}"
+    apt-get -y install openjdk-11-jdk >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "[${red}Error${plain}] Failed to install ${red}openjdk-11-jdk${plain}"
+        exit 1
+    fi
+}
+
+download_files() {
+    echo -e "[${yellow}Info${plain}] Downloading ${green}mirai-console-wrapper-0.3.0.jar${plain} ..."
+    download "mirai-console-wrapper-0.3.0.jar" "https://webplus-cn-hangzhou-s-5e5a2d1c60533f0bea484374.oss-cn-hangzhou.aliyuncs.com/mirai-console-wrapper-0.3.0.jar"
+
+    echo -e "[${yellow}Info${plain}] Downloading ${green}mirai-console-addition-V0.2.3.jar${plain} ..."
+    download "mirai-console-addition-V0.2.3.jar" "https://webplus-cn-hangzhou-s-5e5a2d1c60533f0bea484374.oss-cn-hangzhou.aliyuncs.com/mirai-console-addition-V0.2.3%20%281%29.jar"
+
+    echo -e "[${yellow}Info${plain}] Downloading ${green}mirai-api-http-v1.6.3.jar${plain} ..."
+    download "mirai-api-http-v1.6.3.jar" "https://webplus-cn-hangzhou-s-5e5a2d1c60533f0bea484374.oss-cn-hangzhou.aliyuncs.com/mirai-api-http-v1.6.3.jar"
+}
+
+clean_up_files() {
+    echo -e "[${yellow}Info${plain}] mkdir plugins"
+    mkdir plugins
+    echo -e "[${yellow}Info${plain}] mkdir content"
+    mkdir content
+
+    echo -e "[${yellow}Info${plain}] Copying files ..."
+    cp mirai-console-addition-V0.2.3.jar ./plugins/
+    cp mirai-api-http-v1.6.3.jar ./plugins/
+
+    echo -e "[${yellow}Info${plain}] Cleaning up files ..."
+    rm mirai-console-addition-V0.2.3.jar
+    rm mirai-api-http-v1.6.3.jar
+}
+
+config_mirai() {
+    echo -e " "
+}
+
+start_mirai() {
+    echo -e "[${yellow}Info${plain}] Starting ${green}mirai-console-wrapper${plain} ..."
+    chmod +x ./mirai-console-wrapper-0.3.0.jar
+    java -jar ./mirai-console-wrapper-0.3.0.jar
+}
+
+
+# install process
+install_jdk
+download_files
+clean_up_files
+config_mirai
+start_mirai
